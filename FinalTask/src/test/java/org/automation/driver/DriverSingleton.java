13f88f3a2b355eb4146eeeaccc02aa99759b0175
Volodymyr Bilan <vovakaiman2005@gmail.com>
2024-10-11 16:24:05 +0300

    Finished facade for future tasks

---
+++ FinalTask/src/test/java/org/automation/driver/DriverSingleton.java
@@ -0,0 +1,58 @@
+package org.automation.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.OperaDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverSingleton {
+
+    private static WebDriver driver;
+
+    private DriverSingleton() {
+
+    }
+
+    public static WebDriver getDriver() {
+        if (null == driver) {
+            switch (System.getProperty("browser")) {
+                case "chrome": {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                case "edge": {
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                }
+                case "chromium": {
+                    WebDriverManager.chromiumdriver().setup();
+                    driver = new ChromeDriver();
+                }
+                case "safari": {
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                }
+                case "ie": {
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                }
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+        driver.close();
+    }
+}
