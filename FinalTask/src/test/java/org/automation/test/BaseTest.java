66fc1fdd160c9e1007ad15a267712951b9865e9a
Volodymyr Bilan <vovakaiman2005@gmail.com>
2024-10-25 16:05:59 +0300

    Migrated to JUnit 5 from TestNG

--- FinalTask/src/test/java/org/automation/test/BaseTest.java
+++ FinalTask/src/test/java/org/automation/test/BaseTest.java
@@ -2,22 +2,23 @@ package org.automation.test;
 
 import org.automation.driver.DriverSingleton;
 import org.automation.util.TestListener;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 
-@Listeners({TestListener.class})
+
+@ExtendWith(TestListener.class)
 public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeMethod
+    @BeforeEach
     public void setUp() {
         driver = DriverSingleton.getDriver();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterEach
     public void stopBrowser() {
         DriverSingleton.closeDriver();
     }
